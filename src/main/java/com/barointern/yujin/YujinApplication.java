8de64d239ea9db5059d40db2efbea6eeff8551c1
Hujin0322 <hongyuj0322@naver.com>
2025-02-15 23:55:25 +0900

    chore: 프로젝트 세팅 (Gradle, Spring Boot)

---
+++ src/main/java/com/barointern/yujin/YujinApplication.java
@@ -0,0 +1,13 @@
+package com.barointern.yujin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YujinApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(YujinApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/barointern/yujin/YujinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barointern.yujin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YujinApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
