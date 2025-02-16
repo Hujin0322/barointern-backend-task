c7e960c1011fe66daf6d8e92701f2283f2a090f5
Hujin0322 <hongyuj0322@naver.com>
2025-02-16 21:38:07 +0900

    feat: ErrorCode 추가

--- src/main/java/com/barointern/yujin/presentation/exception/ErrorCode.java
+++ src/main/java/com/barointern/yujin/presentation/exception/ErrorCode.java
@@ -1,5 +1,6 @@
 package com.barointern.yujin.presentation.exception;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,18 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-  DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 username입니다.");
+  DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 username입니다."),
+  LOGIN_FAIL(HttpStatus.BAD_REQUEST, "로그인에 실패했습니다."),
+  INVALID_SECRET_KEY(HttpStatus.INTERNAL_SERVER_ERROR, "SECRET_KEY 초기화에 실패했습니다."),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "일치하는 사용자가 존재하지 않습니다."),
+  USERNAME_EXTRACTION_FAILED(HttpStatus.UNAUTHORIZED, "username 추출에 실패했습니다."),
+  ROLE_EXTRACTION_FAILED(HttpStatus.UNAUTHORIZED, "역할 추출에 실패했습니다."),
+  EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 JWT 토큰입니다."),
+  UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "지원되지 않는 JWT 토큰입니다."),
+  INVALID_TOKEN(HttpStatus.BAD_REQUEST, "잘못된 JWT 토큰입니다."),
+  JWT_VALIDATION_FAILED(HttpStatus.UNAUTHORIZED, "JWT 검증에 실패했습니다.")
+  ;
+
 
   private final HttpStatus httpStatus;
   private final String message;
