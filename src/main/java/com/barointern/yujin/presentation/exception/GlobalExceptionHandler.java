6b5a12cda8830cb66f1c676b34616b69e271313e
Hujin0322 <hongyuj0322@naver.com>
2025-02-16 20:10:24 +0900

    feat: 유효성 검사 실패에 대한 에러 처리 로직 추가

--- src/main/java/com/barointern/yujin/presentation/exception/GlobalExceptionHandler.java
+++ src/main/java/com/barointern/yujin/presentation/exception/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.barointern.yujin.presentation.exception;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -11,12 +15,28 @@ public class GlobalExceptionHandler {
 
   private final String ERROR_LOG = "[ERROR] %s %s";
 
+  // AuthException
   @ExceptionHandler(AuthException.class)
   protected ResponseEntity<ErrorResponse> handleAuthException(AuthException e) {
     log.error(String.format(ERROR_LOG, e.getHttpStatus(), e.getMessage()));
 
     ErrorResponse errorResponse = ErrorResponse.of(e.getHttpStatus(), e.getMessage());
     return new ResponseEntity<>(errorResponse, e.getHttpStatus());
+  }
+
+  // 유효성 검사
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  protected ResponseEntity<ErrorResponse> handleValidationException(
+      MethodArgumentNotValidException e) {
+    log.error(String.format(ERROR_LOG, HttpStatus.BAD_REQUEST, e.getMessage()));
+
+    List<String> errorMessages = e.getBindingResult().getFieldErrors().stream()
+        .map(error -> error.getField() + ": " + error.getDefaultMessage())
+        .collect(Collectors.toList());
+
+    ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "유효성 검사 실패",
+        errorMessages);
+    return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
   }
 
 }
