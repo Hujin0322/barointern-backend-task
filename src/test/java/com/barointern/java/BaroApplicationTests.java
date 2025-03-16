3af8b02bb383c02d88808a024c087d518eed6f8e
Hujin0322 <hongyuj0322@naver.com>
2025-03-16 12:19:44 +0900

    chore: 설정 변경

---
+++ src/test/java/com/barointern/java/BaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barointern.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
